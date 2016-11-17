2680e2bf9a8c57dc07386bbeb549700c11196657
lucas <lucas@145.28.199.60>
2016-11-17 15:22:37 +0100

    bagageformulier+query werkend

--- src/fys/BagageformulierenController.java
+++ src/fys/BagageformulierenController.java
@@ -37,6 +37,7 @@ import javafx.scene.control.TextField;
 public class BagageformulierenController implements Initializable {        
     @FXML private ComboBox airport_combo;
     @FXML private TextField name_input;
+    @FXML private TextField surname_input;
     @FXML private TextField address_input;
     @FXML private TextField residence_input;
     @FXML private TextField zipcode_input;
@@ -56,9 +57,9 @@ public class BagageformulierenController implements Initializable {
     @FXML
     private void handleSendToDatabase(ActionEvent event) throws IOException, SQLException {
         FYS fys = new FYS();
-        //Check if username and password is filled in and correct.
-        //Show error if not filled in or not correct.      
+        
         if((name_input.getText() == null || name_input.getText().trim().isEmpty())
+                || (surname_input.getText() == null || surname_input.getText().trim().isEmpty())
                 || (airport_combo.getValue() == null)
                 || (address_input.getText() == null || address_input.getText().trim().isEmpty())
                 || (residence_input.getText() == null || residence_input.getText().trim().isEmpty())
@@ -70,24 +71,31 @@ public class BagageformulierenController implements Initializable {
         )){
             System.out.println("U heeft niet alles ingevuld!");
         } else{
-            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
             Date date = new Date();
-            String dateString = dateFormat.format(date);
+            String dateTimeString = dateFormat.format(date);    
+            String[] tokens = dateTimeString.split(" ");
+            if (tokens.length != 2) {
+                throw new IllegalArgumentException();
+            }
+            String dateString = tokens[0];
+            String timeString = tokens[1];
+// now do your processing
             
-            sendToDatabase(airport_combo.getValue().toString(), name_input.getText(), 
+            sendToDatabase(airport_combo.getValue().toString(), name_input.getText(), surname_input.getText(), 
                     address_input.getText(), residence_input.getText(), zipcode_input.getText(),
                     country_input.getText(), phone_input.getText(), mail_input.getText(), 
                     account_checkbox.isSelected(), labelnumber_input.getText(), 
                     flightnumber_input.getText(), destination_input.getText(), 
                     type_combo.getValue().toString(), brand_input.getText(), 
-                    color_input.getText(), dateString);
+                    color_input.getText(), dateString, timeString);
         }
     }
     
     @FXML
-    private void sendToDatabase(String airport, String name, String address, String residence, String zipcode, 
-            String country, String phone, String mail, Boolean checkbox, String labelnumber, String flightnumber, 
-            String destination, String type, String brand, String color, String date) throws IOException, SQLException {
+    private void sendToDatabase(String airport, String frontname, String surname, String address, String residence, String zipcode, 
+            String country, String phone, String mail, Boolean checkBox, String labelnumber, String flightnumber, 
+            String destination, String type, String brand, String color, String date, String time) throws IOException, SQLException {
         FYS fys = new FYS();
         
         try {
@@ -98,14 +106,35 @@ public class BagageformulierenController implements Initializable {
             stmt = conn.createStatement();
 
             //connectToDatabase(conn, stmt, "test", "root", "root");
-            String sql_person = "INSERT INTO bagagedatabase.person_table (first_name, address, residence, "
-                    + "zipcode, country, phone, mail) VALUES ('" + name + "', '" + address + "', "
+            String sql_person = "INSERT INTO bagagedatabase.person_table (first_name, surname, address, residence, "
+                    + "zipcode, country, phone, mail) VALUES ('" + frontname + "', '" + surname + "', '" + address + "', "
                     + "'" + residence + "', '" + zipcode + "', '" + country + "', '" + phone + "', "
                     + "'" + mail + "')";
             
-            String sql_airport = "INSERT INTO bagagedatabase.airport_table (date, airport_lost, "
-                    + "label_number, flight_number, destination) VALUES ('" + date + "', '" + airport + "', "
+            String sql_airport = "INSERT INTO bagagedatabase.airport_table (date, time, airport_lost, "
+                    + "label_number, flight_number, destination) VALUES ('" + date + "', '" + time + "', '" + airport + "', "
                     + "'" + labelnumber + "', '" + flightnumber + "', '" + destination + "')";
+            
+            String sql_personID = "SELECT person_id FROM person_table WHERE first_name = '" + frontname + "'"
+                    + "AND surname = '" + surname + "' AND address = '" + address + "'"
+                    + "AND residence = '" + residence + "' AND zipcode = '" + zipcode + "'"
+                    + "AND country = '" + country + "' AND phone = '" + phone + "'"
+                    + "AND mail = '" + mail + "'";
+            
+            String personID = stmt.executeQuery(sql_personID).toString();
+            System.out.println(personID);
+            
+            String sql_lost = "INSERT INTO";
+            
+            if(checkBox){
+                String sql_account = "INSERT INTO bagagedatabase.accounts (type, mail, password, language,"
+                        + "first_name, last_name, address, residence, zip_code, country, phone) VALUES"
+                        + "(0, '" + mail + "', '33225ecb58b9218a', 0, '" + frontname + "', '" + surname + "',"
+                        + "'" + address + "', '" + residence + "', '" + zipcode + "', '" + country + "',"
+                        + "'" + phone + "')";
+                
+                stmt.executeUpdate(sql_account);
+            }
             
             stmt.executeUpdate(sql_person);
             stmt.executeUpdate(sql_airport);

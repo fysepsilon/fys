fe5c5a1f787d8252a073f231d2d91b563168e298
azimi <azimi@145.28.225.111>
2016-12-13 12:34:46 +0100

    Title toegevoegd

--- src/fys/BagagedatabaseController.java
+++ src/fys/BagagedatabaseController.java
@@ -114,7 +114,7 @@ public class BagagedatabaseController implements Initializable {
         type.setText(taal[50]);
         brand.setText(taal[51]);
         date.setText(taal[52]);
-        information.setText(taal[53]);
+        information.setText(taal[23]);
         firstName.setText(taal[9]);
         surName.setText(taal[10]);
         address.setText(taal[11]);
